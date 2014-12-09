897c8cb0f0565fdfc6b87be3692f29e97627b8b1
ubertroll <lennarthoffmann@gmx.de>
2014-12-09 12:09:56 +0100

    ggg

--- Credits.js
+++ Credits.js
@@ -3,7 +3,7 @@
 
     var style = { font: "55px Arial", fill: "#ffffff", align: "center" };
 
-    this.textObject = game.add.text(0, 0, creditsText, style);
+    this.textObject = game.add.text(0, 2000, creditsText, style);
 
     this.tween = game.add.tween(this.textObject).to({ y: -800 }, 4000, Phaser.Easing.Linear.None).loop().start();
 }
